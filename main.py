ee87a48079fd7043368a16b63c5a2cbf6aea9d31
sergio <sergio.s271126@cesurformacion.com>
2021-12-17 19:01:52 +0100

    Prueba de cambios de lineas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     print("Mi primera linea de codigo")
-    print("Mi primera linea de codigo")
-    print("Mi primera linea de codigo")
+    print("Mi segunda linea de codigo")
+    print("Mi cuarta linea de codigo")
